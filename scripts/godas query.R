c941d4387dcbbb40d17de549538f97c88ad50bca
mike.litzow <mike.litzow@noaa.gov>
2020-10-25 20:14:44 -0800

    Computing pollock egg/larvae GODAS temp anomalies

--- scripts/godas query.R
+++ scripts/godas query.R
@@ -61,7 +61,9 @@ URL <- "http://apdrc.soest.hawaii.edu/erddap/griddap/hawaii_soest_d346_28ac_fccf
 # ################################################################################################################
 
 # process
-nc <- nc_open("downloads/X69.161.17.52.281.12.11.48.nc")
+# nc <- nc_open("downloads/X69.161.17.52.281.12.11.48.nc")
+nc <- nc_open("downloads/X161.55.246.46.298.19.41.36.nc") #262m
+
 nc
 
 # extract dates
@@ -76,18 +78,19 @@ lon <- rep(x, each = length(y))   # Vector of longitudes
 ncvar_get(nc, "level")
 
 # will query the data one depth at a time!
-temp1.15 <- ncvar_get(nc, "pottmp") # ºK
-dim(temp.15)
+temp1.262 <- ncvar_get(nc, "pottmp") # ºK
+dim(temp1.262)
 
 # process!
-temp1.15 <- aperm(temp1.15, 3:1)  # First, reverse order of dimensions ("transpose" array)
+temp1.262 <- aperm(temp1.262, 3:1)  # First, reverse order of dimensions ("transpose" array)
 
-temp1.15 <- matrix(temp1.15, nrow=dim(temp1.15)[1], ncol=prod(dim(temp1.15)[2:3]))  # Change to matrix
+temp1.262 <- matrix(temp1.262, nrow=dim(temp1.262)[1], ncol=prod(dim(temp1.262)[2:3]))  # Change to matrix
 
-dimnames(temp1.15) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
+dimnames(temp1.262) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
 
 # now the second half of the TS
-nc <- nc_open("downloads/X69.161.17.52.281.12.12.36.nc")
+# nc <- nc_open("downloads/X69.161.17.52.281.12.12.36.nc")
+nc <- nc_open("downloads/X161.55.246.46.298.19.42.24.nc") # 262 m
 nc
 
 # extract dates
@@ -98,59 +101,59 @@ d2 <- dates(raw, origin = c(1,1,1800))
 ncvar_get(nc, "level")
 
 # will query the data one depth at a time!
-temp2.15 <- ncvar_get(nc, "pottmp") # ºK
+temp2.262 <- ncvar_get(nc, "pottmp") # ºK
 
 # process!
-temp2.15 <- aperm(temp2.15, 3:1)  # First, reverse order of dimensions ("transpose" array)
+temp2.262 <- aperm(temp2.262, 3:1)  # First, reverse order of dimensions ("transpose" array)
 
-temp2.15 <- matrix(temp2.15, nrow=dim(temp2.15)[1], ncol=prod(dim(temp2.15)[2:3]))  # Change to matrix
+temp2.262 <- matrix(temp2.262, nrow=dim(temp2.262)[1], ncol=prod(dim(temp2.262)[2:3]))  # Change to matrix
 
-dimnames(temp2.15) <- list(as.character(d2), paste("N", lat, "E", lon, sep=""))
+dimnames(temp2.262) <- list(as.character(d2), paste("N", lat, "E", lon, sep=""))
 
-temp.15 <- rbind(temp1.15, temp2.15)
+temp.262 <- rbind(temp1.262, temp2.262)
 
 # convert to C
-temp.15 <- temp.15-273.15
+temp.262 <- temp.262-273.262
 
 # drop some of the abyssal area
 drop <- lat < y[6] & lon == 207.5
-temp.15[,drop] <- NA
+temp.262[,drop] <- NA
 
 drop <- lat < y[5] & lon %in% c(206.5, 205.5)
-temp.15[,drop] <- NA
+temp.262[,drop] <- NA
 
 drop <- lat < y[4] & lon == 204.5
-temp.15[,drop] <- NA
+temp.262[,drop] <- NA
 
 drop <- lat < y[3] & lon %in% c(202.5, 203.5)
-temp.15[,drop] <- NA
+temp.262[,drop] <- NA
 
 drop <- lat < y[2] & lon == 201.5
-temp.15[,drop] <- NA
+temp.262[,drop] <- NA
 
 drop <- lat > y[3] & lon == 199.5
-temp.15[,drop] <- NA
+temp.262[,drop] <- NA
 
 # check
-temp.15.mean <- colMeans(temp.15, na.rm=T)
-z <- t(matrix(temp.15.mean,length(y)))  
+temp.262.mean <- colMeans(temp.262, na.rm=T)
+z <- t(matrix(temp.262.mean,length(y)))  
 image.plot(x,y,z, col=tim.colors(64))
 contour(x, y, z, add=T)  
 map('world2Hires',fill=F,xlim=c(130,250), ylim=c(20,66),add=T, lwd=2)
 
-
 # and save the plot
-png("figs/godas temperature query area.png", 5, 4, units="in", res=300)
-temp.mean <- colMeans(temp.105, na.rm=T)
-z <- t(matrix(temp.mean,length(y)))  
-image.plot(x,y,z, col=oceColorsPalette(64), xlim=c(200,215), ylim=c(53,62))
-contour(x, y, z, add=T)  
-map('world2Hires',c('Canada', 'usa'), fill=T,xlim=c(130,250), ylim=c(20,66),add=T, lwd=1, col="lightyellow3")
-dev.off()
+# png("figs/godas temperature query area.png", 5, 4, units="in", res=300)
+# temp.mean <- colMeans(temp.105, na.rm=T)
+# z <- t(matrix(temp.mean,length(y)))  
+# image.plot(x,y,z, col=oceColorsPalette(64), xlim=c(200,215), ylim=c(53,62))
+# contour(x, y, z, add=T)  
+# map('world2Hires',c('Canada', 'usa'), fill=T,xlim=c(130,250), ylim=c(20,66),add=T, lwd=1, col="lightyellow3")
+# dev.off()
 
 
-# now 105m
-nc <- nc_open("downloads/X69.161.17.52.281.12.12.8.nc")
+# now 65m
+# nc <- nc_open("downloads/X69.161.17.52.281.12.12.8.nc")
+nc <- nc_open("downloads/X161.55.246.46.298.19.43.40.nc") # 65m!
 nc
 
 # extract dates
@@ -161,17 +164,18 @@ d <- dates(raw, origin = c(1,1,1800))
 ncvar_get(nc, "level")
 
 # will query the data one depth at a time!
-temp1.105 <- ncvar_get(nc, "pottmp") # ºK
+temp1.65 <- ncvar_get(nc, "pottmp") # ºK
 
 # process!
-temp1.105 <- aperm(temp1.105, 3:1)  # First, reverse order of dimensions ("transpose" array)
+temp1.65 <- aperm(temp1.65, 3:1)  # First, reverse order of dimensions ("transpose" array)
 
-temp1.105 <- matrix(temp1.105, nrow=dim(temp1.105)[1], ncol=prod(dim(temp1.105)[2:3]))  # Change to matrix
+temp1.65 <- matrix(temp1.65, nrow=dim(temp1.65)[1], ncol=prod(dim(temp1.65)[2:3]))  # Change to matrix
 
-dimnames(temp1.105) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
+dimnames(temp1.65) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
 
 # now the second half of the TS
-nc <- nc_open("downloads/X69.161.17.52.281.12.12.52.nc")
+# nc <- nc_open("downloads/X69.161.17.52.281.12.12.52.nc")
+nc <- nc_open("downloads/X161.55.246.46.298.19.44.25.nc") # 65m
 nc
 
 # extract dates
@@ -182,49 +186,126 @@ d2 <- dates(raw, origin = c(1,1,1800))
 ncvar_get(nc, "level")
 
 # will query the data one depth at a time!
-temp2.105 <- ncvar_get(nc, "pottmp") # ºK
+temp2.65 <- ncvar_get(nc, "pottmp") # ºK
 
 # process!
-temp2.105 <- aperm(temp2.105, 3:1)  # First, reverse order of dimensions ("transpose" array)
+temp2.65 <- aperm(temp2.65, 3:1)  # First, reverse order of dimensions ("transpose" array)
 
-temp2.105 <- matrix(temp2.105, nrow=dim(temp2.105)[1], ncol=prod(dim(temp2.105)[2:3]))  # Change to matrix
+temp2.65 <- matrix(temp2.65, nrow=dim(temp2.65)[1], ncol=prod(dim(temp2.65)[2:3]))  # Change to matrix
 
-dimnames(temp2.105) <- list(as.character(d2), paste("N", lat, "E", lon, sep=""))
+dimnames(temp2.65) <- list(as.character(d2), paste("N", lat, "E", lon, sep=""))
 
-temp.105 <- rbind(temp1.105, temp2.105)
+temp.65 <- rbind(temp1.65, temp2.65)
 
 # convert to C
-temp.105 <- temp.105-273.15
+temp.65 <- temp.65-273.15
 
 # drop some of the abyssal area
 drop <- lat < y[6] & lon == 207.5
-temp.105[,drop] <- NA
+temp.65[,drop] <- NA
 
 drop <- lat < y[5] & lon %in% c(206.5, 205.5)
-temp.105[,drop] <- NA
+temp.65[,drop] <- NA
 
 drop <- lat < y[4] & lon == 204.5
-temp.105[,drop] <- NA
+temp.65[,drop] <- NA
 
 drop <- lat < y[3] & lon %in% c(202.5, 203.5)
-temp.105[,drop] <- NA
+temp.65[,drop] <- NA
 
 drop <- lat < y[2] & lon == 201.5
-temp.105[,drop] <- NA
+temp.65[,drop] <- NA
 
 drop <- lat > y[3] & lon == 199.5
-temp.105[,drop] <- NA
+temp.65[,drop] <- NA
 
 # check
-temp.105.mean <- colMeans(temp.105, na.rm=T)
-z <- t(matrix(temp.105.mean,length(y)))  
+temp.65.mean <- colMeans(temp.65, na.rm=T)
+z <- t(matrix(temp.65.mean,length(y)))  
 image.plot(x,y,z, col=tim.colors(64))
 contour(x, y, z, add=T)  
 map('world2Hires',fill=F,xlim=c(130,250), ylim=c(20,66),add=T, lwd=2)
 
+###########################
+# now 25m
+# nc <- nc_open("downloads/X69.161.17.52.281.12.12.8.nc")
+nc <- nc_open("downloads/X161.55.246.46.298.19.45.3.nc") 
+nc
+
+# extract dates
+raw <- ncvar_get(nc, "time") # seconds since 1-1-1970
+d <- dates(raw, origin = c(1,1,1800))
+
+# check depth
+ncvar_get(nc, "level")
+
+# will query the data one depth at a time!
+temp1.25 <- ncvar_get(nc, "pottmp") # ºK
+
+# process!
+temp1.25 <- aperm(temp1.25, 3:1)  # First, reverse order of dimensions ("transpose" array)
+
+temp1.25 <- matrix(temp1.25, nrow=dim(temp1.25)[1], ncol=prod(dim(temp1.25)[2:3]))  # Change to matrix
+
+dimnames(temp1.25) <- list(as.character(d), paste("N", lat, "E", lon, sep=""))
+
+# now the second half of the TS
+# nc <- nc_open("downloads/X69.161.17.52.281.12.12.52.nc")
+nc <- nc_open("downloads/X161.55.246.46.298.19.45.47.nc")
+nc
+
+# extract dates
+raw <- ncvar_get(nc, "time") # seconds since 1-1-1970
+d2 <- dates(raw, origin = c(1,1,1800))
+
+# check depth
+ncvar_get(nc, "level")
+
+# will query the data one depth at a time!
+temp2.25 <- ncvar_get(nc, "pottmp") # ºK
+
+# process!
+temp2.25 <- aperm(temp2.25, 3:1)  # First, reverse order of dimensions ("transpose" array)
+
+temp2.25 <- matrix(temp2.25, nrow=dim(temp2.25)[1], ncol=prod(dim(temp2.25)[2:3]))  # Change to matrix
+
+dimnames(temp2.25) <- list(as.character(d2), paste("N", lat, "E", lon, sep=""))
+
+temp.25 <- rbind(temp1.25, temp2.25)
+
+# convert to C
+temp.25 <- temp.25-273.15
+
+# drop some of the abyssal area
+drop <- lat < y[6] & lon == 207.5
+temp.25[,drop] <- NA
+
+drop <- lat < y[5] & lon %in% c(206.5, 205.5)
+temp.25[,drop] <- NA
+
+drop <- lat < y[4] & lon == 204.5
+temp.25[,drop] <- NA
+
+drop <- lat < y[3] & lon %in% c(202.5, 203.5)
+temp.25[,drop] <- NA
+
+drop <- lat < y[2] & lon == 201.5
+temp.25[,drop] <- NA
+
+drop <- lat > y[3] & lon == 199.5
+temp.25[,drop] <- NA
+
+# check
+temp.25.mean <- colMeans(temp.25, na.rm=T)
+z <- t(matrix(temp.25.mean,length(y)))  
+image.plot(x,y,z, col=tim.colors(64))
+contour(x, y, z, add=T)  
+map('world2Hires',fill=F,xlim=c(130,250), ylim=c(20,66),add=T, lwd=2)
+
+###########################
 
 #################
-# now the monthly anomalies for both!
+# now the monthly anomalies for all three!
 
 d <- c(d, d2)
 m <- months(d)
@@ -233,43 +314,60 @@ yr <- years(d)
 f <- function(x) tapply(x, m[yr %in% 1980:2009], mean)
 ff <- function(x) tapply(x, m[yr %in% 1980:2009], sd)
 
-mu.15 <- apply(temp.15[yr %in% 1980:2009,], 2, f)	# Compute monthly means for each time series (location)
-mu.15 <- mu.15[rep(1:12, floor(length(d)/12)),] 
+mu.25 <- apply(temp.25[yr %in% 1980:2009,], 2, f)	# Compute monthly means for each time series (location)
+mu.25 <- mu.25[rep(1:12, floor(length(d)/12)),] 
 
 xtra <- 12*((length(d)/12)-floor(length(d)/12))
 
-mu.15 <- rbind(mu.15, mu.15[1:xtra,])
+mu.25 <- rbind(mu.25, mu.25[1:xtra,])
 
-sd.15 <- apply(temp.15[yr %in% 1980:2009,], 2, ff)	# Compute monthly sd for each time series (location)
-sd.15 <- sd.15[rep(1:12, floor(length(d)/12)),] 
+sd.25 <- apply(temp.25[yr %in% 1980:2009,], 2, ff)	# Compute monthly sd for each time series (location)
+sd.25 <- sd.25[rep(1:12, floor(length(d)/12)),] 
 
-sd.15 <- rbind(sd.15, sd.15[1:xtra,])
+sd.25 <- rbind(sd.25, sd.25[1:xtra,])
 
 
-anom.15 <- rowMeans((temp.15 - mu.15)/sd.15, na.rm=T)   # Compute matrix of anomalies!
+anom.25 <- rowMeans((temp.25 - mu.25)/sd.25, na.rm=T)   # Compute matrix of anomalies!
 
-# now 105m
-mu.105 <- apply(temp.105[yr %in% 1980:2009,], 2, f)	# Compute monthly means for each time series (location)
-mu.105 <- mu.105[rep(1:12, floor(length(d)/12)),] 
+# now 65m
+mu.65 <- apply(temp.65[yr %in% 1980:2009,], 2, f)	# Compute monthly means for each time series (location)
+mu.65 <- mu.65[rep(1:12, floor(length(d)/12)),] 
 
 xtra <- 12*((length(d)/12)-floor(length(d)/12))
 
-mu.105 <- rbind(mu.105, mu.105[1:xtra,])
+mu.65 <- rbind(mu.65, mu.65[1:xtra,])
 
-sd.105 <- apply(temp.105[yr %in% 1980:2009,], 2, ff)	# Compute monthly sd for each time series (location)
-sd.105 <- sd.105[rep(1:12, floor(length(d)/12)),] 
+sd.65 <- apply(temp.65[yr %in% 1980:2009,], 2, ff)	# Compute monthly sd for each time series (location)
+sd.65 <- sd.65[rep(1:12, floor(length(d)/12)),] 
 
-sd.105 <- rbind(sd.105, sd.105[1:xtra,])
+sd.65 <- rbind(sd.65, sd.65[1:xtra,])
 
 
-anom.105 <- rowMeans((temp.105 - mu.105)/sd.105, na.rm=T)   # Compute matrix of anomalies!
+anom.65 <- rowMeans((temp.65 - mu.65)/sd.65, na.rm=T)   # Compute matrix of anomalies!
+
+# and 262m
+mu.262 <- apply(temp.262[yr %in% 1980:2009,], 2, f)	# Compute monthly means for each time series (location)
+mu.262 <- mu.262[rep(1:12, floor(length(d)/12)),] 
+
+xtra <- 12*((length(d)/12)-floor(length(d)/12))
+
+mu.262 <- rbind(mu.262, mu.262[1:xtra,])
+
+sd.262 <- apply(temp.262[yr %in% 1980:2009,], 2, ff)	# Compute monthly sd for each time series (location)
+sd.262 <- sd.262[rep(1:12, floor(length(d)/12)),] 
+
+sd.262 <- rbind(sd.262, sd.262[1:xtra,])
+
+
+anom.262 <- rowMeans((temp.262 - mu.262)/sd.262, na.rm=T)   # Compute matrix of anomalies!
 
 yr <- as.numeric(as.character(yr))
 dec.yr <- yr + (as.numeric(m)-0.5)/12 
 
 plot.dat <- data.frame(year=dec.yr,
-                       anom.15=anom.15,
-                       anom.105=anom.105)
+                       anom.25=anom.25,
+                       anom.65=anom.65,
+                       anom.262=anom.262)
 
 plot.dat <- plot.dat %>%
   pivot_longer(cols = -year)
@@ -277,31 +375,43 @@ plot.dat <- plot.dat %>%
 ggplot(plot.dat, aes(year, value, color=name)) +
   geom_line() +
   geom_point() +
-  scale_color_manual(values=cb[c(2,4)])
+  scale_color_manual(values=cb[c(2,4,6)])
 
 
 
 ##
-
-# summarize Jan-April 105m
-jan.apr.m <- m[m %in% c("Jan", "Feb", "Mar", "Apr")]
-jan.apr.yr <- yr[m %in% c("Jan", "Feb", "Mar", "Apr")]
-
-
-jan.apr.105 <- anom.105[m %in% c("Jan", "Feb", "Mar", "Apr")]
-jan.apr.105 <- tapply(jan.apr.105, jan.apr.yr, mean)
-
-# and Apr-Jun 15m
-apr.jun.m <- m[m %in% c("Apr", "May", "Jun")]
-apr.jun.yr <- yr[m %in% c("Apr", "May", "Jun")]
+# summarize Mar-May 262m
+mar.may.m <- m[m %in% c("Mar", "Apr", "May")]
+mar.may.yr <- yr[m %in% c("Mar", "Apr", "May")]
 
 
-apr.jun.15 <- anom.15[m %in% c("Apr", "May", "Jun")]
-apr.jun.15 <- tapply(apr.jun.15, apr.jun.yr, mean)
+mar.may.262 <- anom.262[m %in% c("Mar", "Apr", "May")]
+mar.may.262 <- tapply(mar.may.262, mar.may.yr, mean)
+
+# and Mar-Apr 65m
+mar.apr.m <- m[m %in% c("Mar", "Apr")]
+mar.apr.yr <- yr[m %in% c("Mar", "Apr")]
+
+
+mar.apr.65 <- anom.65[m %in% c("Mar", "Apr")]
+mar.apr.65 <- tapply(mar.apr.65, mar.apr.yr, mean)
+
+# and Mar-Apr 25m
+may.jun.m <- m[m %in% c("May", "Jun")]
+may.jun.yr <- yr[m %in% c("May", "Jun")]
+
+
+may.jun.25 <- anom.25[m %in% c("May", "Jun")]
+may.jun.25 <- tapply(may.jun.25, may.jun.yr, mean)
+
+# combine the two shallower TS for larval conditions
+larval.temp <- cbind(mar.apr.65, may.jun.25)
+
+larval <- apply(larval.temp, 1, mean)
 
 godas.anom <- data.frame(year=1980:2020,
-                         apr.jun.15=apr.jun.15,
-                         jan.apr.105=jan.apr.105)
+                         egg=mar.may.262,
+                         larval=larval)
 
 plot.anom <- godas.anom %>%
   pivot_longer(cols=-year)
@@ -310,6 +420,11 @@ ggplot(plot.anom, aes(year, value, color=name)) +
   geom_line() +
   geom_point() +
   scale_color_manual(values=cb[c(2,4)])
+
+# might be enough contrast to distinguish the effects of the two??
+
+cor(godas.anom[godas.anom$year %in% 2006:2020,]) # egg - larval r = 0.40...
+
 
 save.dat <- godas.anom
 
@@ -323,8 +438,64 @@ ggplot(plot.dat, aes(year, value, color=name)) +
   geom_point() +
   scale_color_manual(values=cb[c(2:4)])
 
-write.csv(save.dat, "output data/godas anomalies.csv")
+write.csv(save.dat, "output data/pollock godas anomalies.csv")
 
+
+
+
+# # summarize Jan-April 105m
+# jan.apr.m <- m[m %in% c("Jan", "Feb", "Mar", "Apr")]
+# jan.apr.yr <- yr[m %in% c("Jan", "Feb", "Mar", "Apr")]
+# 
+# 
+# jan.apr.105 <- anom.105[m %in% c("Jan", "Feb", "Mar", "Apr")]
+# jan.apr.105 <- tapply(jan.apr.105, jan.apr.yr, mean)
+# 
+# # and Apr-Jun 15m
+# apr.jun.m <- m[m %in% c("Apr", "May", "Jun")]
+# apr.jun.yr <- yr[m %in% c("Apr", "May", "Jun")]
+# 
+# 
+# apr.jun.15 <- anom.15[m %in% c("Apr", "May", "Jun")]
+# apr.jun.15 <- tapply(apr.jun.15, apr.jun.yr, mean)
+# 
+# godas.anom <- data.frame(year=1980:2020,
+#                          apr.jun.15=apr.jun.15,
+#                          jan.apr.105=jan.apr.105)
+# 
+# plot.anom <- godas.anom %>%
+#   pivot_longer(cols=-year)
+# 
+# ggplot(plot.anom, aes(year, value, color=name)) +
+#   geom_line() +
+#   geom_point() +
+#   scale_color_manual(values=cb[c(2,4)])
+# 
+# save.dat <- godas.anom
+# 
+# save.dat$mean.anom <- apply(save.dat[,2:3], 1, mean)
+# 
+# plot.dat <- save.dat %>%
+#   pivot_longer(cols = -year)
+# 
+# ggplot(plot.dat, aes(year, value, color=name)) +
+#   geom_line() +
+#   geom_point() +
+#   scale_color_manual(values=cb[c(2:4)])
+# 
+# write.csv(save.dat, "output data/godas anomalies.csv")
+
+cod.godas <- read.csv("output data/godas anomalies.csv", row.names = 1)
+
+head(cod.godas)
+
+cod.godas <- cod.godas %>%
+  pivot_longer(cols=-year)
+
+ggplot(cod.godas, aes(year, value, color=name)) +
+  geom_line() +
+  geom_point() +
+  scale_color_manual(values=cb[c(2,4,6)])
 #################
 # old stuff below!
 m2 <- months(d2)
